e07bbe56d133867761c194fb859484222c8fa0f2
Josef Fürlinger <josef.fuerlinger@smartpoint.at>
2019-12-05 14:35:59 +0100

    create template

--- source/NumberGenerator.ConsoleApp/Program.cs
+++ source/NumberGenerator.ConsoleApp/Program.cs
@@ -1,20 +1,13 @@
-﻿using NumberGenerator.ConsoleApp.Observer;
-using NumberGenerator.Logic;
+﻿using NumberGenerator.Logic;
 
 namespace NumberGenerator.ConsoleApp
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             MyNumberGenerator generator = new MyNumberGenerator();
-
-            generator.Attach(new BarView());
-            generator.Attach(new TextView());
-            generator.Attach(new CountView());
-            generator.Attach(new SumView());
-
-            generator.StartGeneration();
+            generator.Start();
         }
     }
 }
--- source/NumberGenerator.Logic/MyNumberGenerator.cs
+++ source/NumberGenerator.Logic/MyNumberGenerator.cs
@@ -1,70 +1,13 @@
-﻿using NumberGenerator.Logic.Contracts;
-using System;
-using System.Collections.Generic;
-using System.Threading.Tasks;
-
-namespace NumberGenerator.Logic
+﻿namespace NumberGenerator.Logic
 {
-    public class MyNumberGenerator : IObservable
+    public class MyNumberGenerator
     {
         private const int RANDOM_MIN_VALUE = 1;
         private const int RANDOM_MAX_VALUE = 10;
+        private const int MSECONDS_TO_SLEEP = 750;
 
-        private readonly IList<IObserver> _observers = new List<IObserver>();
-
-        public void Attach(IObserver observer)
+        public void Start()
         {
-            if (observer == null)
-            {
-                throw new ArgumentNullException(nameof(observer));
-            }
-
-            if (!_observers.Contains(observer))
-            {
-                _observers.Add(observer);
-            }
-            else
-            {
-                throw new InvalidOperationException("Observer is already attached!");
-            }
-        }
-
-        public void Detach(IObserver observer)
-        {
-            if (observer == null)
-            {
-                throw new ArgumentNullException(nameof(observer));
-            }
-
-            if (_observers.Contains(observer))
-            {
-                _observers.Add(observer);
-            }
-            else
-            {
-                throw new InvalidOperationException("Observer is not attached!");
-            }
-        }
-
-        public void StartGeneration()
-        {
-            Random random = new Random();
-
-            while (true)
-            {
-                int nextNumber = random.Next(RANDOM_MIN_VALUE, RANDOM_MAX_VALUE + 1);
-                NewNumberGenerated(nextNumber);
-                Console.WriteLine("--------------------------------");
-                Task.Delay(500).Wait();
-            }
-        }
-
-        private void NewNumberGenerated(int nextNumber)
-        {
-            foreach (IObserver observer in _observers)
-            {
-                observer.Update(nextNumber);
-            }
         }
     }
 }
