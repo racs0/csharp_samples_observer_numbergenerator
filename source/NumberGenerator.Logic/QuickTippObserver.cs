c48db10726d0ddffd0a14de2fc07d33d6b7987fd
Josef Fürlinger <josef.fuerlinger@smartpoint.at>
2019-12-05 18:42:33 +0100

    Template wiederhergestellt

---
+++ source/NumberGenerator.Logic/QuickTippObserver.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NumberGenerator.Logic
+{
+    /// <summary>
+    /// Beobachter, welcher auf einen vollständigen Quick-Tipp wartet: 6 unterschiedliche Zahlen zw. 1 und 45.
+    /// </summary>
+    public class QuickTippObserver : IObserver
+    {
+        #region Fields
+
+        private IObservable _numberGenerator;
+
+        #endregion
+
+        #region Properties
+
+        public List<int> QuickTippNumbers { get; private set; }
+        public int CountOfNumbersReceived { get; private set; }
+
+        #endregion
+
+        #region Constructor
+
+        public QuickTippObserver(IObservable numberGenerator)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        #region Methods
+
+        public void OnNextNumber(int number)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string ToString()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void DetachFromNumberGenerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
---
+++ source/NumberGenerator.Logic/StatisticsObserver.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace NumberGenerator.Logic
+{
+    /// <summary>
+    /// Beobachter, welcher einfache Statistiken bereit stellt (Min, Max, Sum, Avg).
+    /// </summary>
+    public class StatisticsObserver : BaseObserver
+    {
+        #region Fields
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Enthält das Minimum der generierten Zahlen.
+        /// </summary>
+        public int Min { get; private set; }
+
+        /// <summary>
+        /// Enthält das Maximum der generierten Zahlen.
+        /// </summary>
+        public int Max { get; private set; }
+
+        /// <summary>
+        /// Enthält die Summe der generierten Zahlen.
+        /// </summary>
+        public int Sum { get; private set; }
+
+        /// <summary>
+        /// Enthält den Durchschnitt der generierten Zahlen.
+        /// </summary>
+        public int Avg => throw new NotImplementedException();
+
+        #endregion
+
+        #region Constructors
+
+        public StatisticsObserver(IObservable numberGenerator, int countOfNumbersToWaitFor) : base(numberGenerator, countOfNumbersToWaitFor)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        #region Methods
+
+        public override string ToString()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void OnNextNumber(int number)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
